2d8119e09b204f3f295815f0eff6f29186d77c7d
eezn <55613344+eezn@users.noreply.github.com>
2020-08-06 02:57:10 +0900

    #5.2 Lists Model

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,8 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
 ]
 
 
